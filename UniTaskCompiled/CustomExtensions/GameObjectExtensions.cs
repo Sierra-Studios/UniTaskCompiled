dc98874aced237bda750bb3a7eecffdddf126a41
Saskyc <kokoskorp@gmail.com>
2026-08-11 12:27:56 +0200

    extension addition & version bump

---
+++ UniTaskCompiled/CustomExtensions/GameObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace UniTaskCompiled.CustomExtensions
+{
+    #nullable enable
+    public static class GameObjectExtensions
+    {
+        public static CancellationToken GetSaveCancellationToken(this GameObject? gameObject)
+        {
+            if (gameObject == null)
+            {
+                return new CancellationToken(true);
+            }
+            
+            return gameObject.GetCancellationTokenOnDestroy();
+        }
+    }
+}
